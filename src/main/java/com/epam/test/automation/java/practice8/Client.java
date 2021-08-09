925e7949e22f3edaa34cb495ecaff5a6804a07b2
zVvpaTUP <zvvpatup@email.com>
2021-08-09 21:55:09 +0000

    Update Client.java

--- src/main/java/com/epam/test/automation/java/practice8/Client.java
+++ src/main/java/com/epam/test/automation/java/practice8/Client.java
@@ -1,12 +1,14 @@
 package com.epam.test.automation.java.practice8;
 
 import java.math.BigDecimal;
+import java.util.Iterator;
 
-public class Client {
+
+public class Client implements Iterable<Deposit> {
     private static final int DEPOSIT_ARRAY_LENGTH = 10;
     private static final String ILLEGAL_ARGUMENT = "Argument must not be null";
-
     private final Deposit[] deposits;
+
     public Client() {
         this.deposits = new Deposit[DEPOSIT_ARRAY_LENGTH];
     }
@@ -23,27 +25,93 @@ public class Client {
         }
         return false;
     }
+
+    /**
+     *
+     * @return total income amount based on all client’s deposits with %
+     */
     public BigDecimal totalIncome(){
         BigDecimal totInc=BigDecimal.ZERO;
-        for (Deposit depos: deposits) {
-            if (depos !=null)
-                totInc=totInc.add(depos.income());
+        for (Deposit deposit: deposits) {
+            if (deposit !=null)
+                totInc=totInc.add(deposit.income());
         }
         return totInc;
     }
+
+    /**
+     *
+     * @return max deposit income with %
+     */
     public BigDecimal maxIncome(){
         BigDecimal maxIncome=deposits[0].income();
-        for (var depos: deposits) {
-            if (maxIncome.compareTo(depos.income())<0)
-                maxIncome=depos.income();
+        for (var deposit: deposits) {
+            if (deposit!=null){
+                if (maxIncome.compareTo(deposit.income())<0) maxIncome=deposit.income();
             }
+        }
         return maxIncome;
     }
+
+    /**
+     *
+     * @param n - number elements array
+     * @return - income from deposit with such number
+     */
     public BigDecimal getIncomeByNumber(int n){
         if (n<0){
             throw new IllegalArgumentException();
         }
         if (deposits[n]==null) return BigDecimal.ZERO;
         return deposits[n].income();
+    }
+
+
+    @Override
+    public Iterator<Deposit> iterator() {
+        return new depositIterator();
+    }
+    class depositIterator implements Iterator<Deposit>{
+        private int i;
+
+        public depositIterator() {
+            this.i = 0;
+        }
+        @Override
+        public boolean hasNext() {
+            if (deposits[i]==null) return false;
+            return i<=deposits.length-1;
+        }
+        @Override
+        public Deposit next() {
+            return deposits[i++];
+        }
+    }
+
+    public Deposit[] sortDeposits(){
+        Deposit temp;
+        for (int i =deposits.length-1; i >1; i--) {
+            for (int j = 0; j < i; j++) {
+                if (deposits[j+1].incomeAmount().compareTo(deposits[j].incomeAmount())>0){
+                    temp=deposits[j+1];
+                    deposits[j+1]=deposits[j];
+                    deposits[j]=temp;
+                }
+
+            }
+
+        }
+        return deposits;
+    }
+
+    public int countPossibleToProlongDeposit(){
+        int sum=0;
+        for (Deposit deposit:deposits) {
+           // if (deposit instanceof Prolongable)
+            //if (Prolongable.class.isInstance(deposit))
+               if  (((Prolongable) deposit).canToProlong())
+                sum++;
+        }
+        return sum;
     }
 }
