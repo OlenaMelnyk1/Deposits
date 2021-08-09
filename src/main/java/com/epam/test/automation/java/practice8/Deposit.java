2a07eedbc857b5252d9ae7e55309775ff79c74f4
zVvpaTUP <zvvpatup@email.com>
2021-08-09 18:49:59 +0000

    Update Deposit.java

--- src/main/java/com/epam/test/automation/java/practice8/Deposit.java
+++ src/main/java/com/epam/test/automation/java/practice8/Deposit.java
@@ -1,10 +1,31 @@
 package com.epam.test.automation.java.practice8;
 
-/**
- * <summary>
- * Implement class according to description of task.
- * </summary>
- */
-public class Deposit {
-    //TODO: Delete this line and write your own solution;
+import java.math.BigDecimal;
+
+public abstract class Deposit implements Comparable <Deposit> {
+    public final  BigDecimal amount;
+    public final int period;
+    static final String INCORRECT_ARGS= "Incorrect period or amount";
+
+    public Deposit(BigDecimal amount, int period){
+        if ((amount.compareTo(BigDecimal.ZERO)<0) || (period<1) || (amount==null))
+            throw new IllegalArgumentException(INCORRECT_ARGS);
+        this.amount=amount;
+        this.period=period;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    /** Income is the difference between sum amount at the begin and at the end deposit.
+     *
+     * @return
+     */
+    public abstract BigDecimal income();
+
+    public abstract BigDecimal incomeAmount();
+
+
 }
+
